6b7c84c8743a1cb15841481501c4f38962b04779
Luis Maya <luisfelipemayaq@gmail.com>
2024-01-10 18:55:32 -0500

    Parte 7

---
+++ Parte_7.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cargar datos desde el archivo CSV
+df = pd.read_csv("datos_preparados.csv")
+
+# Graficar la distribución de edades con un histograma
+plt.figure(figsize=(8, 6))
+plt.hist(df['edad_categorizada'], bins=5, color='skyblue', edgecolor='black', alpha=0.7)
+plt.title('Distribución de Edades')
+plt.xlabel('Edad Categorizada')
+plt.ylabel('Frecuencia')
+plt.show()
+
+# Graficar histogramas agrupados por hombre y mujer
+grupos = ['anaemia', 'diabetes', 'smoking', 'DEATH_EVENT']
+nombres_grupos = ['Anémicos', 'Diabéticos', 'Fumadores', 'Muertos']
+
+plt.figure(figsize=(12, 8))
+
+for i, grupo in enumerate(grupos):
+    plt.subplot(2, 2, i+1)
+    hombres = df[df['sex'] == 1][grupo]
+    mujeres = df[df['sex'] == 0][grupo]
+
+    plt.bar(hombres.unique(), hombres.value_counts(), width=0.4, label='Hombres', align='edge', alpha=0.7)
+    plt.bar(mujeres.unique(), mujeres.value_counts(), width=-0.4, label='Mujeres', align='edge', alpha=0.7)
+
+    plt.title(f'Distribución de {nombres_grupos[i]} por Género')
+    plt.xlabel(nombres_grupos[i])
+    plt.ylabel('Frecuencia')
+    plt.legend()
+
+plt.tight_layout()
+plt.show()
